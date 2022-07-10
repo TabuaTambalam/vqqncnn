e02e806f2948a920cb3eb40afc40576a16dbc697
TabuaTambalam <95035059+TabuaTambalam@users.noreply.github.com>
2022-07-10 23:48:10 +0800

    Update setupncnn.sh

--- setupncnn.sh
+++ setupncnn.sh
@@ -1,3 +1,4 @@
 pip3 install ncnn
 wget -O /tmp/vq.param https://raw.githubusercontent.com/TabuaTambalam/vqqncnn/main/vq.param
 wget -O /tmp/vq.bin https://github.com/TabuaTambalam/vqqncnn/releases/download/0.0/vq.bin
+wget https://github.com/TabuaTambalam/vqqncnn/releases/download/0.1/oz.bin
