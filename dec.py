d61dcf6df1b98a2a0aa8234f57e373a865feef26
TabuaTambalam <95035059+TabuaTambalam@users.noreply.github.com>
2022-07-29 18:43:35 +0800

    Update dec.py

--- dec.py
+++ dec.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 from PIL import Image
 import ncnn
@@ -7,11 +8,17 @@ net.load_param("/tmp/vq.param")
 net.load_model("/tmp/vq.bin")
 
 dumped_seqs=np.fromfile('oz.bin',dtype=np.uint16).astype(np.int32).reshape((-1,256))
-
+#np.random.shuffle(dumped_seqs)
 k=0
-for seq in dumped_seqs:
+
+try:
+	k=int(sys.argv[1])
+except:
+	pass
+
+for seq in dumped_seqs[k:]:
   with net.create_extractor() as ex:
     ex.input("in0", ncnn.Mat(seq).clone())
     hrr, out0 = ex.extract("out0")
-  Image.fromarray(np.array(out0).astype(np.uint8)).save('%d.png'%k)
+  Image.fromarray(np.array(out0).astype(np.uint8)).save('/tmp/%d.png'%k)
   k+=1
