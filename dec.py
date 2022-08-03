0fc5fc1e6e17d14189dd88359b84c02713750bae
TabuaTambalam <95035059+TabuaTambalam@users.noreply.github.com>
2022-08-03 18:04:04 +0800

    Update dec.py

--- dec.py
+++ dec.py
@@ -7,8 +7,7 @@ net.opt.use_vulkan_compute = True
 net.load_param("/tmp/vq.param")
 net.load_model("/tmp/vq.bin")
 
-dumped_seqs=np.fromfile('oz.bin',dtype=np.uint16).astype(np.int32).reshape((-1,256))
-#np.random.shuffle(dumped_seqs)
+seqbin='oz.bin'
 k=0
 
 try:
@@ -16,9 +15,17 @@ try:
 except:
 	pass
 
+try:
+	seqbin=sys.argv[2]+'.bin'
+except:
+	pass
+
+dumped_seqs=np.fromfile(seqbin,dtype=np.uint16).astype(np.int32).reshape((-1,256))
+#np.random.shuffle(dumped_seqs)
+
 for seq in dumped_seqs[k:]:
   with net.create_extractor() as ex:
     ex.input("in0", ncnn.Mat(seq).clone())
     hrr, out0 = ex.extract("out0")
-  Image.fromarray(np.array(out0).astype(np.uint8)).save('/tmp/%d.png'%k)
+  Image.fromarray(np.array(out0).astype(np.uint8)).save('%d.png'%k)
   k+=1
