de587e6e161a390da997cf1500f191f7d16f71ce
TabuaTambalam <95035059+TabuaTambalam@users.noreply.github.com>
2022-08-11 22:33:13 +0800

    Update vqdecjit.py

--- vqdecjit.py
+++ vqdecjit.py
@@ -13,9 +13,9 @@ def showp(n, prt=False):
   global mlat
   global mfn
   daaz=dumped_seqs[n]
-  #out0 = vqdecjit.vqdec(torch.flatten(torch.from_numpy(daaz)),vqdecjit.vqd_shape)
-  out0 = vqdec(torch.flatten(torch.from_numpy(daaz)),vqd_shape)
-  uz=Image.fromarray(out0.numpy().astype(np.uint8))
+  #out0 = vqdec(torch.flatten(torch.from_numpy(daaz.astype(np.int32)).to(mindd.device) ),vqd_shape)
+  out0 = vqdecjit.vqdec(torch.flatten(torch.from_numpy(daaz.astype(np.int32)).to(mindd.device) ),vqdecjit.vqd_shape)
+  uz=Image.fromarray(out0.cpu().numpy().astype(np.uint8))
   uz.save('/content/sample_data/%d.png'%n)
   if prt:
     display(uz)
@@ -26,8 +26,8 @@ def showp(n, prt=False):
     return uz
 
 def showp2(seq):
-  out0 = vqdec(torch.flatten(torch.from_numpy(seq)),vqd_shape)
-  uz=Image.fromarray(out0.numpy().astype(np.uint8))
+  out0 = vqdec(torch.flatten(torch.from_numpy(seq).to(mindd.device)),vqd_shape)
+  uz=Image.fromarray(out0.cpu().numpy().astype(np.uint8))
   uz.save('/content/sample_data/000.png')
   return uz
 
